8bf69480dbfef537b533194465f6b05f18f71f41
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-12 21:43:15 +0300

    We redraw one component using React. memo

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,18 +1,12 @@
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
-import {
-  Box,
-  Button,
-  FormControl,
-  IconButton,
-  TextField,
-} from '@material-ui/core';
+import { IconButton, TextField } from '@material-ui/core';
 import { AddBox } from '@material-ui/icons';
 
 type AddItemFormPropsType = {
   addItem: (title: string) => void;
 };
 
-export function AddItemForm(props: AddItemFormPropsType) {
+export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
   let [title, setTitle] = useState('');
   let [error, setError] = useState<string | null>(null);
 
@@ -30,7 +24,9 @@ export function AddItemForm(props: AddItemFormPropsType) {
   };
 
   const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    setError(null);
+    if (error !== null) {
+      setError(null);
+    }
     if (e.charCode === 13) {
       addItem();
     }
@@ -53,4 +49,4 @@ export function AddItemForm(props: AddItemFormPropsType) {
       </IconButton>
     </div>
   );
-}
+});
