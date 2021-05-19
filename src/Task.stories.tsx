8a5d7b6d5977defb1f936f38ec221cf2cac80e71
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-19 22:32:05 +0300

    Made Task stories

---
+++ src/Task.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Task, TaskPropsType } from './Task';
+
+export default {
+  title: 'Todolist/Task',
+  component: Task,
+} as Meta;
+
+const removeTaskCallback = action('Remove Button inside Task clicked ');
+const changeTaskStatusCallback = action(' Status changed inside Task');
+const changeTaskTitleCallback = action('Title changed inside Task');
+
+const Template: Story<TaskPropsType> = (args) => <Task {...args} />;
+
+const baseArgs = {
+  removeTask: removeTaskCallback,
+  changeTaskStatus: changeTaskStatusCallback,
+  changeTaskTitle: changeTaskTitleCallback,
+};
+
+export const TaskIsDoneExample = Template.bind({});
+TaskIsDoneExample.args = {
+  ...baseArgs,
+  task: { id: '1', isDone: true, title: 'JS' },
+};
+
+export const TaskIsNotDoneExample = Template.bind({});
+TaskIsNotDoneExample.args = {
+  ...baseArgs,
+  task: { id: '1', isDone: false, title: 'JS' },
+};
