bf7e0a3110852af3ea3390cd99019ac3c12304a3
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-03 22:21:48 +0300

    Update aplication(delete storybook, snapshots), and update name of variables

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,52 +1,50 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
-import { IconButton, TextField } from '@material-ui/core';
-import { AddBox } from '@material-ui/icons';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Box, Button, FormControl, IconButton, TextField} from '@material-ui/core';
+import {AddBox} from '@material-ui/icons';
 
-export type AddItemFormPropsType = {
-  addItem: (title: string) => void;
-};
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
 
-export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
-  let [title, setTitle] = useState('');
-  let [error, setError] = useState<string | null>(null);
+export const AddItemForm = React.memo(function(props: AddItemFormPropsType) {
+    console.log("AddItemForm called")
 
-  const addItem = () => {
-    if (title.trim() !== '') {
-      props.addItem(title);
-      setTitle('');
-    } else {
-      setError('Title is required');
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
     }
-  };
 
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value);
-  };
-
-  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (error !== null) {
-      setError(null);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
     }
-    if (e.charCode === 13) {
-      addItem();
-    }
-  };
 
-  return (
-    <div>
-      <TextField
-        style={{ marginBottom: '10px' }}
-        variant="outlined"
-        error={!!error}
-        value={title}
-        onChange={onChangeHandler}
-        onKeyPress={onKeyPressHandler}
-        label="Title"
-        helperText={error}
-      />
-      <IconButton color="primary" onClick={addItem}>
-        <AddBox />
-      </IconButton>
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null);
+        }
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   label="Title"
+                   helperText={error}
+        />
+        <IconButton color="primary" onClick={addItem}>
+            <AddBox />
+        </IconButton>
     </div>
-  );
-});
+})
